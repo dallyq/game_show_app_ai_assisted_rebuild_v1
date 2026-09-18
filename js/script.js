cf38ac3c13d8b1abb8f5889bed592cf7f4435deb
Dallas Quin <166860264+dallyq@users.noreply.github.com>
2026-09-18 13:13:26 +0200

    Added game setup for event listener and global variables, icons and submitting hiding the overlay.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
 const qwerty = document.getElementById("qwerty");
 const word = document.getElementById("word");
 let missed = 0;
+
+const overlay = document.getElementById("overlay");
+const setupForm = document.getElementById("setup-form");
+const scoreboard = document.getElementById("scoreboard");
+
+let difficulty;
+let lifeIcon;
+
+setupForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  difficulty = document.getElementById("difficulty").value;
+  lifeIcon = document.getElementById("lifeicon").value;
+
+  const lifeImages = scoreboard.querySelectorAll("img");
+  lifeImages.forEach((img) => {
+    img.src = `./images/live${lifeIcon}.png`;
+  });
+
+  overlay.style.display = "none";
+});
